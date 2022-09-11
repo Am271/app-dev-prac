05f01f3a8e6bbf2c3cdb65f62b60fab642f0e737
Amitash <amitu895@gmail.com>
2022-09-11 22:14:43 +0530

    Starting new Activity using Intent and Chained layout for flexible components

---
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    // A constant to use as a key for Intents
+    public static final String EXTRA_MESSAGE = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+//         creating a new Intent, the current context and the activity to which the intent is supplied
+//         are taken as parameters
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        An identifier is created to reference the editable text field
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        The value of the text field is taken and stored in the message
+        String message = editText.getText().toString();
+//        The variable is then passed to the Intent to carry it to the next activity
+        intent.putExtra(EXTRA_MESSAGE, message);
+//        The new activity is started with the intent
+        startActivity(intent);
+    }
+}
